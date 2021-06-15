0cc7004a3948f173b256521e854a3fd2bb903bc5
Geet Choubey <gkchoubery@myseneca.ca>
2021-06-15 03:14:01 -0400

    integrated heroku

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const express = require('express');
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults();
 
+const PORT = process.env.PORT || 3000;
+
 server.use(express.static('build'));
 server.use(middlewares);
 server.use('/api', router);
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('JSON Server is running')
 });
